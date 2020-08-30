ef3bc30e9e4398334b83fa6d869f645402a1c6e3
Smit <smitshah95@hotmail.com>
2020-08-30 15:49:08 +0800

    Updated Contract tests

--- src/main/java/com/techtrek/customerservice/config/SecurityConfiguration.java
+++ src/main/java/com/techtrek/customerservice/config/SecurityConfiguration.java
@@ -2,9 +2,11 @@ package com.techtrek.customerservice.config;
 
 import com.techtrek.customerservice.config.user_security.ApplicationUserRole;
 import com.techtrek.customerservice.config.user_security.ApplicationUserServiceAdapter;
+import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
@@ -14,20 +16,20 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+@Profile("!default")
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true)
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
-
     @Autowired
-    private ApplicationUserServiceAdapter applicationUserServiceAdapter;
+    private DaoAuthenticationProvider daoAuthenticationProvider;
 
     @Override
     protected void configure(HttpSecurity httpSecurity) throws Exception {
         httpSecurity
                 .csrf().disable()
                 .authorizeRequests()
-                .antMatchers("/", "index" ,"/css/*", "/js/*", "/actuator/**")
+                .antMatchers("/", "index" ,"/css/*", "/js/*", "/actuator/**", "/area/**")
                 .permitAll()
                 .anyRequest()
                 .authenticated()
@@ -37,20 +39,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.authenticationProvider(daoAuthenticationProvider());
-    }
-
-    @Bean
-    public DaoAuthenticationProvider daoAuthenticationProvider() {
-        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
-        provider.setPasswordEncoder(passwordEncoder());
-        provider.setUserDetailsService(applicationUserServiceAdapter);
-        return provider;
-    }
-
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder(10);
+        auth.authenticationProvider(daoAuthenticationProvider);
     }
 
 }
--- src/test/java/com/techtrek/customerservice/math_resource/MathResourceBaseContract.java
+++ src/test/java/com/techtrek/customerservice/math_resource/MathResourceBaseContract.java
@@ -1,15 +1,23 @@
 package com.techtrek.customerservice.math_resource;
 
-import com.techtrek.customerservice.resource.BaseTestClass;
+import com.techtrek.customerservice.CustomerServiceApplication;
+import com.techtrek.customerservice.config.user_security.ApplicationUserServiceAdapter;
 import io.restassured.RestAssured;
-import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 @ExtendWith(SpringExtension.class)
-@SpringBootTest(classes = AreaCalculationController.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@WithMockUser(value = "spring")
+@SpringBootTest(classes = CustomerServiceApplication.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class MathResourceBaseContract {
 
     private static final String HTTP_LOCALHOST = "http://localhost:";
@@ -17,7 +25,7 @@ public class MathResourceBaseContract {
     @LocalServerPort
     private Integer port;
 
-    @BeforeAll
+    @BeforeEach
     public void setup() {
         RestAssured.baseURI = HTTP_LOCALHOST + port;
     }
