2a7a726813d2648f2caf121199f032281a9cbd13
Smit <smitshah95@hotmail.com>
2020-09-05 15:17:09 +0800

    Updated coveralls.yml

--- src/test/java/com/techtrek/customerservice/CustomerServiceApplicationTests.java
+++ src/test/java/com/techtrek/customerservice/CustomerServiceApplicationTests.java
@@ -1,13 +1,26 @@
 package com.techtrek.customerservice;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
 class CustomerServiceApplicationTests {
 
+	@Autowired
+	private CustomerServiceApplication customerServiceApplication;
+
 	@Test
 	void contextLoads() {
+		assertNotNull(customerServiceApplication);
+	}
+
+	@Test
+	void applicationContextTest(){
+		assertDoesNotThrow(() -> CustomerServiceApplication.main(new String[]{}));
 	}
 
 }
