6eb5dccd4355e4cdaafbe7230b400a69468d6cf8
Smit <smitshah95@hotmail.com>
2020-09-05 15:04:15 +0800

    Updated travis.yml

--- src/main/java/com/techtrek/customerservice/customer/Customer.java
+++ src/main/java/com/techtrek/customerservice/customer/Customer.java
@@ -4,9 +4,6 @@ import lombok.*;
 
 @Getter
 @Builder
-@ToString
-@NoArgsConstructor
-@AllArgsConstructor
 public class Customer {
     private String customerId;
     private String fullName;
@@ -15,20 +12,4 @@ public class Customer {
     private String address;
     private String joinedDate;
     private String dateOfBirth;
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-
-        Customer customer = (Customer) o;
-
-        if (!customerId.equals(customer.customerId)) return false;
-        if (!fullName.equals(customer.fullName)) return false;
-        if (!firstName.equals(customer.firstName)) return false;
-        if (!lastName.equals(customer.lastName)) return false;
-        if (!address.equals(customer.address)) return false;
-        if (!joinedDate.equals(customer.joinedDate)) return false;
-        return dateOfBirth.equals(customer.dateOfBirth);
-    }
 }
--- src/test/java/com/techtrek/customerservice/customer/CustomerProfileServiceAdapterTest.java
+++ src/test/java/com/techtrek/customerservice/customer/CustomerProfileServiceAdapterTest.java
@@ -3,9 +3,7 @@ package com.techtrek.customerservice.customer;
 import com.techtrek.customerservice.test_data_provider.CustomerTestDataProvider;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.Mockito;
+import org.mockito.*;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.List;
@@ -26,13 +24,25 @@ public class CustomerProfileServiceAdapterTest {
     @Mock
     private CustomerQueryRepo customerQueryRepo;
 
+    @Captor
+    ArgumentCaptor<Customer> customerArgumentCaptor;
+
     @Test
     public void testShouldSaveCustomer(){
         Customer customer = buildCustomer();
 
         customerProfileServiceAdapter.addCustomer(customer);
 
-        Mockito.verify(customerCommandRepo, times(1)).addCustomer(eq(customer));
+        Mockito.verify(customerCommandRepo, times(1)).addCustomer(customerArgumentCaptor.capture());
+
+        Customer customer1 = customerArgumentCaptor.getValue();
+        assertEquals(customer.getAddress(), customer1.getAddress());
+        assertEquals(customer.getCustomerId(), customer1.getCustomerId());
+        assertEquals(customer.getDateOfBirth(), customer1.getDateOfBirth());
+        assertEquals(customer.getFirstName(), customer1.getFirstName());
+        assertEquals(customer.getLastName(), customer1.getLastName());
+        assertEquals(customer.getJoinedDate(), customer1.getJoinedDate());
+        assertEquals(customer.getFullName(), customer1.getFullName());
     }
 
     @Test
@@ -41,7 +51,16 @@ public class CustomerProfileServiceAdapterTest {
 
         customerProfileServiceAdapter.updateCustomer(customer.getCustomerId(), customer);
 
-        Mockito.verify(customerCommandRepo, times(1)).updateCustomer(eq(customer.getCustomerId()), eq(customer));
+        Mockito.verify(customerCommandRepo, times(1)).updateCustomer(eq(customer.getCustomerId()),customerArgumentCaptor.capture());
+
+        Customer customer1 = customerArgumentCaptor.getValue();
+        assertEquals(customer.getAddress(), customer1.getAddress());
+        assertEquals(customer.getCustomerId(), customer1.getCustomerId());
+        assertEquals(customer.getDateOfBirth(), customer1.getDateOfBirth());
+        assertEquals(customer.getFirstName(), customer1.getFirstName());
+        assertEquals(customer.getLastName(), customer1.getLastName());
+        assertEquals(customer.getJoinedDate(), customer1.getJoinedDate());
+        assertEquals(customer.getFullName(), customer1.getFullName());
     }
 
     @Test
