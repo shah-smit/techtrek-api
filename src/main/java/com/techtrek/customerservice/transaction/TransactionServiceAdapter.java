b0e6c3c89a8df22dc99ca15f351e8726f618cd8e
Smit <smitshah95@hotmail.com>
2020-10-01 23:46:29 +0800

    Fixed #15 Added Basic null validation and throwing runtime exception

--- src/main/java/com/techtrek/customerservice/transaction/TransactionServiceAdapter.java
+++ src/main/java/com/techtrek/customerservice/transaction/TransactionServiceAdapter.java
@@ -12,6 +12,9 @@ public class TransactionServiceAdapter implements TransactionService{
 
     @Override
     public void addTransaction(Transaction transaction) {
+        if(transaction.getCustomerId() == null) throw new RuntimeException("CustomerId cannot be empty");
+        if(transaction.getAmount() <= 0) throw new RuntimeException("Amount cannot be empty or zero");
+        if(transaction.getFromAccountId() == null) throw new RuntimeException("Account ID cannot be empty");
         transaction.setLocalDateTime();
         transaction.setTransactionId();
 
